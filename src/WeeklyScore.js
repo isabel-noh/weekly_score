535d08af0bea733855175064232b33afae72a597
isabel-noh <nohhyunjeong93@gmail.com>
2022-03-24 16:01:37 +0900

    Update WeeklyScore.js
    랜덤 숫자 받아서 해당 회수만큼 원 색칠하기

--- src/WeeklyScore.js
+++ src/WeeklyScore.js
@@ -4,13 +4,13 @@ import styled from 'styled-components'
 import { Link } from 'react-router-dom';
 
 const WeeklyScore = (props) => {
-    const [scoreM, setScoreM] = React.useState(0);  //초기화 score 값 5 // useState : 함수형 컴포넌트 
-    const [scoreT, setScoreT] = React.useState(0);
-    const [scoreW, setScoreW] = React.useState(0);
-    const [scoreTH, setScoreTH] = React.useState(0);
-    const [scoreF, setScoreF] = React.useState(0);
-    const [scoreSAT, setScoreSAT] = React.useState(0);
-    const [scoreS, setScoreS] = React.useState(0);
+    // const [scoreM, setScoreM] = React.useState(0);  //초기화 score 값 5 // useState : 함수형 컴포넌트 
+    // const [scoreT, setScoreT] = React.useState(0);
+    // const [scoreW, setScoreW] = React.useState(0);
+    // const [scoreTH, setScoreTH] = React.useState(0);
+    // const [scoreF, setScoreF] = React.useState(0);
+    // const [scoreSAT, setScoreSAT] = React.useState(0);
+    // const [scoreS, setScoreS] = React.useState(0);
 
     const day_text_dict = {
         0: "일",
@@ -21,53 +21,64 @@ const WeeklyScore = (props) => {
         5: "금",
         6: "토",
       };
+      console.log(Object.keys(day_text_dict).map((d, i) => day_text_dict[d]));
 
-    const set_score = Array.from({length: 5}, (v, i) => i);
-
-    useEffect(() => {
-        setScoreM(Math.floor(Math.random() * 5) + 1);
-        setScoreT(Math.floor(Math.random() * 5) + 1);
-        setScoreW(Math.floor(Math.random() * 5) + 1);
-        setScoreTH(Math.floor(Math.random() * 5) + 1);
-        setScoreF(Math.floor(Math.random() * 5) + 1);
-        setScoreSAT(Math.floor(Math.random() * 5) + 1);
-        setScoreS(Math.floor(Math.random() * 5) + 1);
-    }, [])
+      const day_text_arr = Object.values(day_text_dict);
+      console.log(day_text_arr)
+    // const set_score = Array.from({length: 5}, (v, i) => i);
 
     // useEffect(() => {
-    //     const score = () => {
-    //         set_score.map((n, i) => {
-    //             Math.floor(Math.random * 5) + 1;
-    //         });
-    //     };
+    //     setScoreM(Math.floor(Math.random() * 5) + 1);
+    //     setScoreT(Math.floor(Math.random() * 5) + 1);
+    //     setScoreW(Math.floor(Math.random() * 5) + 1);
+    //     setScoreTH(Math.floor(Math.random() * 5) + 1);
+    //     setScoreF(Math.floor(Math.random() * 5) + 1);
+    //     setScoreSAT(Math.floor(Math.random() * 5) + 1);
+    //     setScoreS(Math.floor(Math.random() * 5) + 1);
     // }, [])
 
-    // console.log(score);
+    // const monday_score = Array.from({length: scoreM}, (v, i) => i);
+    // const tuesday_score = Array.from({length: scoreT}, (v, i) => i);
+    // const wednes_score = Array.from({length: scoreW}, (v, i) => i);
+    // const thurs_score = Array.from({length: scoreTH}, (v, i) => i);
+    // const friday_score = Array.from({length: scoreF}, (v, i) => i);
+    // const satur_score = Array.from({length: scoreSAT}, (v, i) => i);
+    // const sunday_score = Array.from({length: scoreS}, (v, i) => i);
+    
+    // const random_score = () => {
+    //     return (
+    //         Math.floor(Math.random() * 5) + 1
+    //     )
+    // }
 
-    const monday_score = Array.from({length: scoreM}, (v, i) => i);
-    const tuesday_score = Array.from({length: scoreT}, (v, i) => i);
-    const wednes_score = Array.from({length: scoreW}, (v, i) => i);
-    const thurs_score = Array.from({length: scoreTH}, (v, i) => i);
-    const friday_score = Array.from({length: scoreF}, (v, i) => i);
-    const satur_score = Array.from({length: scoreSAT}, (v, i) => i);
-    const sunday_score = Array.from({length: scoreS}, (v, i) => i);
-
+    const score = day_text_arr.map((v, i) => {
+        return {
+            day: v,
+            score: Math.floor(Math.random() * 5) + 1}
+    })
+    console.log(score)
 
     return (
         <>
         <MyWrap>
             <h1>내 일주일은?</h1>
             <MyWeek>
-                <MyDays>
-                    <div>월</div> 
-                    {monday_score.map((n, i) => {
-                        return (
-                            <div key={i} className='circle'></div>
-                        )
-                    })}
-                    <Link to="/daily/월"><div className='triangle'></div></Link>
-                </MyDays>
-                <MyDays>
+                {score.map((d, i) => {
+                    return (
+                        <MyDays key={i}>
+                            <div>{`${d.day}`}</div> 
+                            {Array.from({length: 5},(v, i) => {
+                                return (
+                                    <Circle key={i}
+                                        style={{backgroundColor: d.score < i+1 ? "#eee": "#f55354"}}></Circle>
+                                )
+                            })}
+                            <Link to="/daily/월"><div className='triangle'></div></Link>
+                        </MyDays>
+                    )
+                })}
+                
+                {/* <MyDays>
                     <div>화</div>
                     {tuesday_score.map((n, i) => {
                         return (
@@ -120,7 +131,7 @@ const WeeklyScore = (props) => {
                         )
                     })}
                     <Link to="/daily/일"><div className='triangle'></div></Link>
-                </MyDays>
+                </MyDays> */}
             </MyWeek>
         </MyWrap>
         </>
@@ -139,6 +150,16 @@ const MyWrap = styled.div`
     h1{
         color: #f55354;
     }
+`
+const Circle = styled.div`
+
+    width : 30px;
+    height : 30px;
+    border-radius: 50%;
+
+    float: left;
+    margin: 0px 5px;
+    background-color: #f55354;
 `
 const MyWeek = styled.div`
     width: 300px;
