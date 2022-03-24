b492deb0470d862e0a17145f19c8d3a54ce3c8c4
Isabel Noh <89833631+isabel-noh@users.noreply.github.com>
2022-03-24 22:03:15 +0900

    Update App.js
    주석 추가

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
   
   return (
     <div className="App">
+    //Switch : 위에서부터 아래로 전체 페이지 route를 확인하는데 위에서 일치하는 것이 없으면 NotFound페이지로 연결됨
       <Switch>
         <Route  path="/" exact >
           <WeeklyScore/>
